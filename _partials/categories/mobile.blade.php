8a1ed506f555437c73b459cdc35787e7b9dbe66c
mindflowgo <the@mindflow.network>
2024-06-13 02:16:45 +0000

    Removed All Categories menu item

--- _partials/categories/mobile.blade.php
+++ _partials/categories/mobile.blade.php
@@ -1,11 +1,11 @@
 <div class="layout-scrollable w-100">
     <ul id="_menu_bar" class="nav nav-categories nav-inline flex-nowrap py-2 w-100">
-        <li class="nav-item">
+        <!-- <li class="nav-item">
             <a
                 class="nav-link text-nowrap font-weight-bold{{ $selectedCategory ? '' : ' active' }}"
                 href="{{ page_url('local/menus', ['category' => null]) }}"
             >@lang('igniter.local::default.text_all_categories')</a>
-        </li>
+        </li> -->
 
         @partial('@items', ['categories' => $categories->toFlatTree(), 'displayAsFlatTree' => true])
     </ul>
