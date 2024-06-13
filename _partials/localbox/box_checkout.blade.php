8e1c146aa0bd5b624aad211e186244ad5d914e6d
mindflowgo <the@mindflow.network>
2024-06-13 10:37:07 +0000

    Fixed checkout UI, map works, buttons arranged

--- _partials/localbox/box_checkout.blade.php
+++ _partials/localbox/box_checkout.blade.php
@@ -7,20 +7,23 @@
             <div class="map_box_checkout1">
                 <div class="map_box_checkout2">
                     <div class="map_box_checkout3">
-                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4846.038163982976!2d-73.1183352326104!3d7.125028222912976!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e68156184ae472b%3A0x523950692525fe10!2sTastyz%C3%B3n%20Los%20Tejaditos!5e0!3m2!1ses-419!2sco!4v1662337010823!5m2!1ses-419!2sco" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                        <iframe
+                    src="https://maps.google.com/maps?q={!! format_address($locationCurrent->getAddress(), FALSE) !!}&output=embed"
+                    width="100%" height="100%" frameborder="0" style="border:0" allowfullscreen></iframe>
+                        <!-- <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4846.038163982976!2d-73.1183352326104!3d7.125028222912976!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e68156184ae472b%3A0x523950692525fe10!2sTastyz%C3%B3n%20Los%20Tejaditos!5e0!3m2!1ses-419!2sco!4v1662337010823!5m2!1ses-419!2sco" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe> -->
                     </div>
                 </div>
             </div>
-            <div class="button_box_checkout">
+            <div class="pt-2 d-flex w-100">
                 <div class="button_box_checkout2">
                     @partial('localBox::control')
                 </div>
+                <div class="ms-auto gbuttons">
+                    <div class="me-2 local-timeslot">
+                        @partial('localBox::timeslot')
+                    </div>
+                </div>
             </div>
         </div>
-      <div class="asap_delivery">
-        <div class="button_box_checkout2">
-            @partial('localBox::timeslot')
-        </div>
-      </div>
     </div>
   </div>
